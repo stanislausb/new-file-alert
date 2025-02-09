2d5b1b342d0d83dde7029faa10ea55bca4de7c58
stanislausb <stan.beijer@gmail.com>
2025-02-09 15:50:49 +0100

    webhook url leeg gemaakt

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 root_dir = ""
-webhook_url = {"HOR" : "https://chat.googleapis.com/v1/spaces/AAAAcC9bd4E/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=DAXptDnuqukqiwEAiCcTkc0DnOtPqOiXLsrjQ0se4Xk", "ABC" : "gekke url"}
+webhook_url = { "HOR" : "",
+                "ABC" : "gekke url"}
